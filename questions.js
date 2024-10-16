59f41a3fa5346a8223bb94541b13c25f9de4f45a
Eduardo Oliveira <eduardocavallin2005@gmail.com>
2024-10-15 21:50:26 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    question: "Qual é a principal causa de acidentes fatais envolvendo pedestres em áreas urbanas?",
+    answers: [
+      { option: "a) Falta de sinalização nas ruas", correct: false },
+      { option: "b) Uso do celular ao atravessar a rua", correct: false },
+      { option: "c) Excesso de velocidade dos veículos", correct: false },
+      { option: "d) Travessia em áreas não sinalizadas", correct: true },
+    
+    ],
+  },
+  {
+    question: "Qual das seguintes ações é considerada uma prática segura para pedestres ao atravessar a rua?",
+    answers: [
+      { option: "a) Atravessar rapidamente fora da faixa de pedestres", correct: false },
+      { option: "b) Olhar para os dois lados antes de atravessar", correct: true },
+      { option: "c) Confiar que os motoristas sempre vão parar", correct: false },
+      { option: "d) Caminhar em grupos grandes para maior segurança", correct: false },
+    ],
+  },
+  {
+    question: "Qual medida pode reduzir significativamente o risco de atropelamento de pedestres à noite?",
+    answers: [
+      { option: "a) Usar roupas escuras", correct: false },
+      { option: "b) Caminhar no meio da rua", correct: false },
+      { option: "c) Usar roupas com materiais refletivos", correct: true },
+      { option: "d) Atravessar em qualquer lugar, desde que esteja atento", correct: false },
+    ],
+  },
+  {
+    question: "Em relação aos motoristas, qual comportamento pode ajudar a proteger a segurança dos pedestres?",
+    answers: [
+      { option: "a) Aumentar a velocidade ao se aproximar de uma faixa de pedestres", correct: false },
+      { option: "b) Respeitar os limites de velocidade e sinais de trânsito", correct: true },
+      { option: "c) Tocar a buzina ao se aproximar de pedestres", correct: false },
+      { option: "d) Acelerar para evitar pedestres atravessando fora da faixa", correct: false },
+    ],
+  },
+];
